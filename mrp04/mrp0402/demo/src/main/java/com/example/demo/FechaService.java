96c2a39b40144c17f3e54de41f2fe6f4f6126ecf
MiguelRodriguezPerez <147710727+MiguelRodriguezPerez@users.noreply.github.com>
2023-10-18 10:48:26 +0200

    Update FechaService.java

--- mrp04/mrp0402/demo/src/main/java/com/example/demo/FechaService.java
+++ mrp04/mrp0402/demo/src/main/java/com/example/demo/FechaService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class FechaService {
     public int desdeUno(LocalDate ld){
-        return ld.getDayOfYear()-(LocalDate.of(LocalDate.now().getYear(),1,1)).getDayOfYear();
+        return ld.getDayOfYear()-1;
     }
     public int entreDosFechas(LocalDate ld1, LocalDate ld2){
         return ld1.getDayOfYear()-ld2.getDayOfYear();

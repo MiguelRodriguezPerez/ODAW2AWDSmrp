7e1cdaa3cba98902d6809a076871d6803d92deff
MiguelRodriguezPerez <147710727+MiguelRodriguezPerez@users.noreply.github.com>
2023-10-20 13:11:50 +0200

    Add files via upload

---
+++ mrp04/mrp0403/demo/src/main/java/com/example/demo/CalculosFolder/CalculosServiceImpl.java
@@ -0,0 +1,61 @@
+package com.example.demo.CalculosFolder;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.TreeSet;
+
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+@Service
+public class CalculosServiceImpl implements CalculosService{
+
+     public boolean obtPrimo(String num,Model model){
+    /*@RequestParam requerirá un valor para ser introducido en la url con el nombre que se           
+      indique en el paréntesis, String num es como se referirá el controlador internamente
+      a ese valor y Model model es necesario para luego devolver los valores al html*/
+        boolean esPrimo= true;//Necesario para saber si el número es primo
+        Integer numero = 0;
+        if (numero == 0|| numero == 1 || numero == 4) {
+            esPrimo=false;//Si el numero es 0,1 o 4 no es primo
+        }
+        else if(esPrimo){
+            for (int i = 2; i < numero / 2; i++) {
+            if (numero % i == 0)esPrimo=false;
+            // Si es divisible por cualquiera de estos números, no es primo
+            }
+        }
+        return esPrimo;
+      }
+
+    public Double obtHipotenusa(Integer X, Integer Y){
+        if(X<0||Y<0) return (double)-1;
+        return Math.pow(X,2)+Math.pow(Y,2);
+    }
+
+    public TreeSet<Integer> obtCalculosSinRepetidos(Integer X){
+        TreeSet<Integer> resultado = new TreeSet<>();
+        if(X<1||X>100){
+            resultado.add(-1); 
+            return resultado;
+        } 
+        Random r = new Random();
+        int adjudicar=0;
+        for(int i = 0; i<X;i++){
+            adjudicar=r.nextInt(100)+1;
+            while(resultado.contains(adjudicar)){adjudicar=r.nextInt(100)+1;}
+            //Evita que haya iteraciones en las que no se introduzcan números
+            resultado.add(adjudicar);
+      }
+      return resultado;
+    }
+
+    public ArrayList<Integer> obtDivisores(int X){
+      ArrayList<Integer> resultado = new ArrayList<>();
+  
+      for(int i = 1; i<X;i++){
+        if(X%i==0)resultado.add(i);//Si el número i es divisor exacto de 0, se añadirá
+        //al  resultado final
+      }
+      return resultado;
+    }
+}
---
+++ mrp04/mrp0403/demo/src/main/java/com/example/demo/FechasFolder/FechasController.java
@@ -0,0 +1,48 @@
+package com.example.demo.FechasFolder;
+
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+@RequestMapping("/fechas")
+public class FechasController {
+    @Autowired
+    FechasService fechaService;
+
+    @GetMapping("/{f1}")
+    public String show2B(@PathVariable LocalDate f1,Model model){
+        model.addAttribute("f1Parametro", f1);
+        model.addAttribute("f1Resultado",fechaService.desdeUno(f1));
+        return "index";
+    }
+    @GetMapping("/{f2}/{f3}")
+    public String show2C(@PathVariable LocalDate f2, @PathVariable LocalDate f3, Model model){
+        model.addAttribute("f2Parametro", f2);
+        model.addAttribute("f3Parametro", f3);
+        model.addAttribute("f2Resultado",fechaService.entreDosFechas(f2, f3));
+        return "index";
+    }
+    @GetMapping("/")
+    public String show2D(Model model){
+        model.addAttribute("f3Resultado", fechaService.desdeUno(LocalDate.now()));
+        return "index";
+    }
+    @GetMapping("/bisiesto/{f4}")
+    public String show2E(@PathVariable LocalDate f4,Model model){
+        model.addAttribute("f4Parametro", f4);
+        model.addAttribute("f4Resultado", fechaService.esBisiesto(f4));
+        return "index";
+    }
+    @GetMapping("/bisiesto/{alloUno}/{alloDos}")
+    public String show2F(@PathVariable Integer alloUno,@PathVariable Integer alloDos,Model model){
+        model.addAttribute("alloUnoParametro",alloUno);
+        model.addAttribute("alloDosParametro", alloDos);
+        model.addAttribute("alloResultado", fechaService.esBisiestoEntreDos(alloUno, alloDos));
+        return "index";
+    }
+}

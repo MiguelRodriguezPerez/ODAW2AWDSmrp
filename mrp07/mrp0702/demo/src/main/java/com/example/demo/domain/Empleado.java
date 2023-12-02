f1d2cf45d967ad894c2ef21fa7df8ea7c5c465df
MiguelRodriguezPerez <147710727+MiguelRodriguezPerez@users.noreply.github.com>
2023-12-02 20:43:45 +0100

    Add files via upload

---
+++ mrp07/mrp0702/demo/src/main/java/com/example/demo/domain/Empleado.java
@@ -0,0 +1,39 @@
+package com.example.demo.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of="id")
+@Entity
+
+public class Empleado{
+  @Id
+  @GeneratedValue
+  @Min(value = 0)
+  private Long id;
+
+  @NotEmpty
+  private String nombre;
+  @Email(message = "Debe tener email valido")
+  private String email;
+  private Double salario;
+  private boolean enActivo;
+  private Genero genero;
+  
+  @ManyToOne
+  // @JoinColumn(name = "DEPTO_ID", foreignKey = @ForeignKey(name="DEPTO_ID_FK"))
+  private Departamento departamento;
+}
